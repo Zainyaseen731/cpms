4387ccc645da4bc880dc34f0a84a1f7c4c10b3da
Zain yaseen <zainyaseen731@gmail.com>
2021-01-15 02:45:18 +0500

    Project setup done

---
+++ src/main/java/com/example/cpms/CpmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.cpms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CpmsApplication.class, args);
+    }
+
+}
